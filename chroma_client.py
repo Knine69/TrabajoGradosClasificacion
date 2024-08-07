bc9bf990d9613141895f3c57e532839cfe793242
Juan Huguet <jhuguetospina@gmail.com>
2024-08-06 19:57:34 -0500

    REFACTOR: method calling

--- chroma_client.py
+++ chroma_client.py
@@ -1,5 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 from chromadb.api.types import Document
+from chromadb.api.models.Collection import Collection
 from chromadb import Documents, EmbeddingFunction, Embeddings
 
 import torch
@@ -7,9 +8,15 @@ import chromadb
 import fitz
 
 
+llm_model = AutoModelForMaskedLM.from_pretrained("bert-base-multilingual-cased", output_hidden_states=True)
+
+
 class EmbedderFunction(EmbeddingFunction):
     tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
-    model = AutoModelForMaskedLM.from_pretrained("bert-base-multilingual-cased", output_hidden_states=True)
+    
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = llm_model
     
     def __call__(self, input: Documents) -> Embeddings:
         embedding_results = []
@@ -54,26 +61,33 @@ def pdf_to_bytes(pdf_path):
     return text_bytes
 
 
+def basic_chroma_query(collection: Collection) -> None:
+    results = collection.query(
+        n_results=10,
+        query_texts=sample_doc,
+        where_document={"$contains": "input entered"}
+        ).values()
+
+    print(f"Results are: {results}")
+
+
+def add_document_embbeds(collection: Collection, document: bytes):
+    collection.add(
+        documents=[sample_doc],
+        metadatas=[{"sample": "1", "page": "1"}],
+        ids=["id1"]
+    )
+    
 # Initialize ChromaDB client and create a collection with the embedding function
 chroma_client = chromadb.Client()
 collection = chroma_client.create_collection(name="my_collection", embedding_function=EmbedderFunction())
 
-
+# Prepare text data in PDF file/s
 pdf_text = pdf_to_bytes('test_file.pdf')
 sample_doc = pdf_text.decode('utf-8')
 
 # Add the document to the collection
-collection.add(
-    documents=[sample_doc],
-    metadatas=[{"sample": "1", "page": "1"}],
-    ids=["id1"]
-)
+add_document_embbeds(collection, sample_doc)
 
 # Query the collection
-results = collection.query(
-    n_results=10,
-    query_texts=sample_doc,
-    where_document={"$contains": "input entered"}
-).values()
-
-print(f"Results are: {results}")
+basic_chroma_query(collection)
