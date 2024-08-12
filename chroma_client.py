f144c76feef65a898eb0c24830d5d66108477433
Juan Huguet <jhuguetospina@gmail.com>
2024-08-12 07:59:35 -0500

    REFACTOR: doc reading and agent creation

--- chroma_client.py
+++ chroma_client.py
@@ -1,14 +1,20 @@
-from transformers import AutoTokenizer, AutoModelForMaskedLM
+from transformers import AutoTokenizer
 from chromadb.api.types import Document
 from chromadb.api.models.Collection import Collection
 from chromadb import Documents, EmbeddingFunction, Embeddings
+
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_community.llms.ollama import Ollama
+
+from tools.tools import tools
+from user_langchain.prompt import prompt
 
 import torch
 import chromadb
 import fitz
 
 
-llm_model = AutoModelForMaskedLM.from_pretrained("bert-base-multilingual-cased", output_hidden_states=True)
+llm_model = Ollama(model="llama2")
 
 
 class EmbedderFunction(EmbeddingFunction):
@@ -30,8 +36,7 @@ class EmbedderFunction(EmbeddingFunction):
         
             with torch.no_grad():
                 outputs = self.model(**inputs)
-            
-            print(f"Outputs are: {outputs}")
+
             hidden_states = outputs.hidden_states
             
             
@@ -61,10 +66,10 @@ def pdf_to_bytes(pdf_path):
     return text_bytes
 
 
-def basic_chroma_query(collection: Collection) -> None:
+def basic_chroma_query(collection: Collection, document: bytes) -> None:
     results = collection.query(
         n_results=10,
-        query_texts=sample_doc,
+        query_texts=[document],
         where_document={"$contains": "input entered"}
         ).values()
 
@@ -73,21 +78,34 @@ def basic_chroma_query(collection: Collection) -> None:
 
 def add_document_embbeds(collection: Collection, document: bytes):
     collection.add(
-        documents=[sample_doc],
+        documents=[document],
         metadatas=[{"sample": "1", "page": "1"}],
         ids=["id1"]
     )
     
-# Initialize ChromaDB client and create a collection with the embedding function
-chroma_client = chromadb.Client()
-collection = chroma_client.create_collection(name="my_collection", embedding_function=EmbedderFunction())
 
-# Prepare text data in PDF file/s
-pdf_text = pdf_to_bytes('test_file.pdf')
-sample_doc = pdf_text.decode('utf-8')
+if __name__ == "__main__":
+    # Initialize ChromaDB client and create a collection with the embedding function
+    chroma_client = chromadb.Client()
+    collection = chroma_client.create_collection(name="my_collection", embedding_function=EmbedderFunction())
 
-# Add the document to the collection
-add_document_embbeds(collection, sample_doc)
+    # Prepare text data in PDF file/s
+    pdf_text = pdf_to_bytes('test_file.pdf')
+    sample_doc = pdf_text.decode('utf-8')
 
-# Query the collection
-basic_chroma_query(collection)
+    # Add the document to the collection
+    add_document_embbeds(collection, sample_doc)
+
+    # Query the collection
+    basic_chroma_query(collection)
+
+    # Initialize Langchain Agent
+    agent = create_react_agent(llm_model, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+    result = agent_executor.invoke(
+        {
+            "input": "What is the length of the word 'fish'"
+        }
+    )
+    
+    print(f'Query result is: {result['output']}')
