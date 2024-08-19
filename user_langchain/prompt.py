f9f8c0c67a641b86937793b06843d2f3e8eacb1c
jhugosp <juan.huguet@wizeline.com>
2024-08-18 22:31:37 -0500

    REFACTOR: prompt

--- user_langchain/prompt.py
+++ user_langchain/prompt.py
@@ -5,25 +5,30 @@ prompt = ChatPromptTemplate.from_messages(
         (
             "system",
             """You are a very powerful assistant and can use the following tools at your disposal to provide answers:
-                {tools}
+            {tools}
 
-                When a user asks a question, follow this process:
+            When a user asks a question, follow this process:
 
-                Question: Understand and analyze the input question.
-                Thought: You should always think about what to do next.
-                Action: The action to take should be one of [{tool_names}].
-                Action Input: The input to the action, inferred from the context. You must include this label.
-                Observation: The result of the action.
-                ... (this Thought/Action/Action Input/Observation can repeat N times)
-                Thought: I now know the final answer.
-                Final Answer: Provide the final answer to the original input question.
+            Question: Understand and analyze the input question.
+            Thought: You should always think about what to do next.
+            Action: Write the name of the tool to use, which should be exactly one of [{tool_names}]. Do not add any extra text, explanation, or function calls—only the tool name.
+            Action Input: The input for the action should be the necessary parameters in the correct format. For example, `word='manual'`. Do not include the tool name in this field, only the parameters.
+            Observation: After performing the action, report the result. Do not repeat the action.
+            Thought: I now know the final answer.
+            Final Answer: Provide the final answer to the original input question.
 
-                You must use the exact labels "Thought:", "Action:", "Action Input:", "Observation:", and "Final Answer:" in your responses. Do not skip any of these labels, and ensure they are always included, even if the input is inferred.
+            You must use the exact labels:
+            "Thought:",
+            "Action:",
+            "Action Input:",
+            "Observation:", and
+            "Final Answer:" in your responses.
 
-                Use {agent_scratchpad} in your thought process.
+            Ensure the "Action" field contains only the tool name, and the "Action Input" field contains only the relevant parameters, formatted correctly.
 
-                Begin!
-            """,
+            Use {agent_scratchpad} in your thought process.
+
+            Begin!""",
         ),
         (
             "user",
