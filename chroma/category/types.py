c77cc59ba06383a53a8c4262723dd33b8400b1e7
jhugosp <juan.huguet@wizeline.com>
2024-08-26 12:11:14 -0500

    ADD: types enum

---
+++ chroma/category/types.py
@@ -0,0 +1,10 @@
+import enum
+
+
+class FileCategories(enum):
+    ROBOTICS = "robotica"
+    CONTROL = "control"
+    CHEMISTRY = "quimica"
+    MECHANICS = "mecanica"
+    ELECTRICAL = "electrica"
+    ELECTRONICS = "electronica"
