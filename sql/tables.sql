0d21320ff4561931d5c0693d8774d0b68ce2fda6
jhugosp <juan.huguet@wizeline.com>
2024-09-03 18:07:05 -0500

    DELETE: unnecessary file

--- sql/tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE DATABASE PdfEmbeddings;
-
-CREATE TABLE IF EXISTS FileItem(
-    id INTEGER NOT NULL KEY AUTOINCREMENT,
-    title varchar(200) NOT NULL,
-    uploaded_date DATETIME NOT NULL,
-    file_source TEXT NOT NULL,
-    active boolean not null
-)
