82e392aa483bb0e7b816342993b75578c196d613
jhugosp <juan.huguet@wizeline.com>
2024-09-04 14:49:32 -0500

    REFACTOR: received files

--- user_langchain/flask/controller/agent.py
+++ user_langchain/flask/controller/agent.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Blueprint, request, jsonify
 from utils.request_validator import validate_params
 from user_langchain.flask.domain.agent_invocations import LangchainAgent
@@ -10,9 +12,12 @@ langchain_agent = LangchainAgent()
 
 @langchain_router.post("/search")
 def search_query():
-    categories = request.args.getlist('categories')
-    if validate_params(categories):
-        return jsonify(langchain_agent.execute_agent_query(categories))
+    request_data = json.loads(request.data.decode('utf-8'))
+    categories = request_data.get('categories', False)
+    documents = request_data.get('documents', False)
+    if validate_params(categories, documents):
+        return jsonify(
+            langchain_agent.execute_agent_query(categories, documents))
 
     return jsonify({
         "STATE": "ERROR",
