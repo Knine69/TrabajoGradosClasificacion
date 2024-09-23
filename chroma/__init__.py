71b563e0e66382426113eaeecead8c49bdc82a38
jhugosp <juan.huguet@wizeline.com>
2024-09-23 11:41:46 -0500

    REFACTOR: method utilization

--- chroma/__init__.py
+++ chroma/__init__.py
@@ -2,17 +2,6 @@ from flask import Flask
 from chroma.app_config import Configuration
 from chroma.app.controller.chroma_client import chroma_router
 from chroma.celery_conf import celery, celery_instantiation
-import redis
-
-
-def test_redis_connection():
-    try:
-        client = redis.StrictRedis(host='localhost', port=6379, db=0)
-        response = client.ping()
-        if response:
-            print("Successfully connected to Redis!")
-    except redis.ConnectionError as e:
-        print(f"Redis connection error: {e}")
 
 
 def create_app():
@@ -20,5 +9,5 @@ def create_app():
     app.config.from_object(Configuration())
     celery_instantiation(app)
     app.register_blueprint(chroma_router)
-    test_redis_connection()
+
     return app
