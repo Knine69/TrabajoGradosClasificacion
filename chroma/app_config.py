ef28929028c0b085ccfc640680c98f266adc1289
jhugosp <juan.huguet@wizeline.com>
2024-09-23 12:47:36 -0500

    REFACTOR: broker env variable name

--- chroma/app_config.py
+++ chroma/app_config.py
@@ -7,5 +7,5 @@ load_dotenv(os.path.join(basedir, '..', '.env'))
 
 class Configuration:
 
-    CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+    RABBIT_BROKER_URL = os.getenv('RABBIT_BROKER_URL')
     CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
