b7695b4876f9bd0de1ecf4aa41b8e296563d9e41
jhugosp <juan.huguet@wizeline.com>
2024-09-23 22:47:45 -0500

    REFACTOR:langchain celery and redis instantiation

--- user_langchain/__init__.py
+++ user_langchain/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from user_langchain.flask.controller.agent import langchain_router
+from app_config import Configuration
+from user_langchain.celery_conf import celery_instantiation, celery
+from user_langchain.app.controller.agent import langchain_router
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(Configuration())
+    celery_instantiation(app)
     app.register_blueprint(langchain_router)
     return app
