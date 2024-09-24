20e2d84c007bdfaba1eed3e20d2eb2ddd5547072
jhugosp <juan.huguet@wizeline.com>
2024-09-23 22:49:43 -0500

    ADD: global get request data

--- chroma/app/controller/chroma_client.py
+++ chroma/app/controller/chroma_client.py
@@ -1,7 +1,7 @@
 from chroma.app.domain.chroma_collections import ChromaCollections
-from task_executor.task_executor import (chroma_search_query_task,
-                                         sse_stream)
-from utils.request_validator import validate_params
+from chroma.app.task_executor import (chroma_search_query_task,
+                                      sse_stream)
+from utils.request_validator import (validate_params, get_request_data)
 from flask import Blueprint, request, jsonify, Response
 import json
 
@@ -12,9 +12,9 @@ chroma_collections = ChromaCollections()
 @chroma_router.route('/task-status', methods=['POST'])
 def receive_task_status():
     data = json.loads(request.data.decode('utf-8'))
-    (task_status, task_result) = _get_request_data(data,
-                                                   'status',
-                                                   'result')
+    (task_status, task_result) = get_request_data(data,
+                                                  'status',
+                                                  'result')
 
     print(f"Received task status: {task_status}")
     print(f"Task result: {task_result}")
@@ -29,15 +29,14 @@ def execute_basic_chroma_query():
     (search_text,
      category,
      collection_name,
-     callback_url) = _get_request_data(request_data,
-                                       'search_text',
-                                       'category',
-                                       'collection_name',
-                                       'callback_url')
+     callback_url) = get_request_data(request_data,
+                                      'search_text',
+                                      'category',
+                                      'collection_name',
+                                      'callback_url')
 
     if validate_params(
             collection_name, category, search_text):
-        print("PARAMS VALIDATED, ENTERING TASK INITIALIZATION")
         task = chroma_search_query_task.apply_async(
             args=[collection_name, category, search_text],)
         return Response(sse_stream(task.id), content_type='text/event-stream')
@@ -52,7 +51,7 @@ def execute_basic_chroma_query():
 def process_pdf_file():
     request_data = json.loads(request.data.decode('utf-8'))
 
-    file_path, categories, collection_name = _get_request_data(
+    file_path, categories, collection_name = get_request_data(
         request_data,
         'file_path',
         'categories',
@@ -63,16 +62,6 @@ def process_pdf_file():
         return jsonify(chroma_collections.process_pdf_file(
             file_path, categories, collection_name
         ))
-
-
-def _get_request_data(request_data, *names):
-    return tuple(
-        request_data.get(
-            name,
-            'http://localhost:5000/chroma/task-status' if name == 'callback_url'
-            else False)
-        for name in names
-    )
 
 # TODO: access pdf directory Try bind mounts
 
--- utils/request_validator.py
+++ utils/request_validator.py
@@ -3,3 +3,13 @@ def validate_params(*args) -> bool:
         if not arg:
             return False
     return True
+
+
+def get_request_data(request_data, *names):
+    return tuple(
+        request_data.get(
+            name,
+            'http://localhost:5000/chroma/task-status' if name == 'callback_url'
+            else False)
+        for name in names
+    )
