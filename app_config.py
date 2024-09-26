3327079561eca9f9e0ecdf486fc7af3bd7789d10
jhugosp <juan.huguet@wizeline.com>
2024-09-26 10:27:55 -0500

    REFACTOR: default values for queues

--- app_config.py
+++ app_config.py
@@ -9,5 +9,5 @@ class Configuration:
 
     RABBIT_BROKER_URL = os.getenv('RABBIT_BROKER_URL')
     CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
-    LANGCHAIN_QUEUE = os.getenv('LANGCHAIN_QUEUE')
-    CHROMA_QUEUE = os.getenv('CHROMA_QUEUE')
+    LANGCHAIN_QUEUE = os.getenv('LANGCHAIN_QUEUE', 'langchain_queue')
+    CHROMA_QUEUE = os.getenv('CHROMA_QUEUE', 'chroma_queue')
