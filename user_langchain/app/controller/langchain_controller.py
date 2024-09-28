560855424d6868a299e41beaff911d3e123b4c9f
jhugosp <juan.huguet@wizeline.com>
2024-09-28 18:57:08 -0500

    REFACTOR: task id management

--- user_langchain/app/controller/langchain_controller.py
+++ user_langchain/app/controller/langchain_controller.py
@@ -13,13 +13,20 @@ langchain_router = Blueprint('langchain',
 @langchain_router.post("/search")
 def search_query():
     request_data = json.loads(request.data.decode('utf-8'))
-    (categories, documents, user_query) = get_request_data(request_data,
-                                                           'categories',
-                                                           'documents',
-                                                           'user_query')
+
+    (categories,
+     documents,
+     user_query,
+     task_id) = get_request_data(request_data,
+                                 'categories',
+                                 'documents',
+                                 'user_query',
+                                 'task_id')
+
     if validate_params(categories, documents):
         task = langchain_agent_invocation_task.apply_async(
             args=[categories, documents, user_query],
+            task_id=task_id,
             queue=Configuration.LANGCHAIN_QUEUE)
         return Response(sse_stream(task.id), content_type='text/event-stream')
 
