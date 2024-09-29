3b9450e208df144a08c06456e59559e613f613d7
jhugosp <juan.huguet@wizeline.com>
2024-09-29 17:48:19 -0500

    REFACTOR: dependencies installation

--- user_langchain/initialize_langchain_services.sh
+++ user_langchain/initialize_langchain_services.sh
@@ -1,6 +1,4 @@
 #!/bin/bash_files
 
-source .langchain/bin/activate
-pip install -r user_langchain/requirements.txt
 gunicorn -c user_langchain/gunicorn.conf.py 'user_langchain:create_app()' &\
 celery -A user_langchain worker --loglevel=info --time-limit=50 --soft-time-limit=30 -Q langchain_queue --hostname=langchain
