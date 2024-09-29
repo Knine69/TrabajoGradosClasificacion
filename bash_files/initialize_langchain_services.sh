f2d2b2e47b6ae818a9a1a33da2b922609087ef14
jhugosp <juan.huguet@wizeline.com>
2024-09-28 20:42:12 -0500

    REFACTOR: hostname in celery

--- bash_files/initialize_langchain_services.sh
+++ bash_files/initialize_langchain_services.sh
@@ -3,4 +3,4 @@
 source .langchain/bin/activate
 pip install -r user_langchain/requirements.txt
 gunicorn -c user_langchain/gunicorn.conf.py 'user_langchain:create_app()' &\
-celery -A user_langchain worker --loglevel=info --time-limit=50 --soft-time-limit=30 -Q langchain_queue
+celery -A user_langchain worker --loglevel=info --time-limit=50 --soft-time-limit=30 -Q langchain_queue --hostname=langchain
