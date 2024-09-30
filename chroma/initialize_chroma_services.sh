5deef1063d46465e74ad7896529e57e98bd8a78c
jhugosp <juan.huguet@wizeline.com>
2024-09-30 11:03:27 -0500

    REFACTOR: line

--- chroma/initialize_chroma_services.sh
+++ chroma/initialize_chroma_services.sh
@@ -1,5 +1,4 @@
 #!/bin/bash_files
 
 gunicorn -c chroma/gunicorn.conf.py 'chroma:create_app()' && \
-celery -A chroma worker --loglevel=info --time-limit=50 --soft-time-limit=30 \
--Q chroma_queue --hostname=chroma@%h
+celery -A chroma worker --loglevel=info --time-limit=50 --soft-time-limit=30 -Q chroma_queue --hostname=chroma@%h
