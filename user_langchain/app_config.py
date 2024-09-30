c7accbd903c41744b09ff06c0d33596fabec5bc2
jhugosp <juan.huguet@wizeline.com>
2024-09-29 19:04:39 -0500

    REFACTOR: default config values

--- user_langchain/app_config.py
+++ user_langchain/app_config.py
@@ -7,7 +7,9 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Configuration:
 
-    RABBIT_BROKER_URL = os.getenv('RABBIT_BROKER_URL')
-    CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
+    RABBIT_BROKER_URL = os.getenv('RABBIT_BROKER_URL',
+                                  'pyamqp://guest@rabbitmq//')
+    CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND',
+                                      'redis://redis:6379/0')
     LANGCHAIN_QUEUE = os.getenv('LANGCHAIN_QUEUE', 'langchain_queue')
     CHROMA_QUEUE = os.getenv('CHROMA_QUEUE', 'chroma_queue')
