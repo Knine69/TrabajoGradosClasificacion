cc00f3d430ba017e4914213554748b791c28f2dc
jhugosp <juan.huguet@wizeline.com>
2024-10-01 19:50:30 -0500

    REFACTOR: file indication

--- bash_files/update_docker_files.sh
+++ bash_files/update_docker_files.sh
@@ -1,14 +1,12 @@
 echo "Building and updating Chroma Docker Image..."
 
-cd chroma/ || exit
-docker build -t chroma-executor .
+docker build -f chroma/Dockerfile -t chroma-executor .
 docker tag chroma-executor jhuguet/chroma-executor
 docker push jhuguet/chroma-executor
 cd ..
 
 echo "Building and updating Langchain Docker Image..."
 
-cd user_langchain/ || exit
-docker build -t langchain-executor .
+docker build -f user_langchain/Dockerfile -t langchain-executor .
 docker tag langchain-executor jhuguet/langchain-executor
 docker push jhuguet/langchain-executor
