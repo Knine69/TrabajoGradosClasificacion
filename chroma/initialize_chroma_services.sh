39cace8a9adbc0984ff08f276d68f89016aaaf75
jhugosp <juan.huguet@wizeline.com>
2024-10-04 16:18:57 -0500

    REFACTOR: sh files

--- chroma/initialize_chroma_services.sh
+++ chroma/initialize_chroma_services.sh
@@ -1,5 +1,5 @@
 #!/bin/bash_files
 
 source /chroma_app/.chroma/bin/activate
-gunicorn -c chroma/gunicorn.conf.py 'chroma:create_app()' && \
+gunicorn -c chroma/gunicorn.conf.py 'chroma:create_app()' & \
 celery -A chroma worker --loglevel=info --time-limit=50 --soft-time-limit=30 -Q chroma_queue --hostname=chroma@%h
--- user_langchain/initialize_langchain_services.sh
+++ user_langchain/initialize_langchain_services.sh
@@ -1,5 +1,5 @@
 #!/bin/bash_files
 
 source /langchain_app/.langchain/bin/activate
-gunicorn -c user_langchain/gunicorn.conf.py 'user_langchain:create_app()' &\
+gunicorn -c user_langchain/gunicorn.conf.py 'user_langchain:create_app()' & \
 celery -A user_langchain worker --loglevel=info --time-limit=50 --soft-time-limit=30 -Q langchain_queue --hostname=langchain
