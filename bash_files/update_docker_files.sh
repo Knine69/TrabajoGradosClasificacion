dfa7742d5c4a818f1e642871f0bcaf8c79951d92
jhugosp <juan.huguet@wizeline.com>
2024-10-03 22:59:31 -0500

    REFACTOR: platform specified

--- bash_files/update_docker_files.sh
+++ bash_files/update_docker_files.sh
@@ -4,12 +4,12 @@ mkdir chroma_server
 
 echo "Building and updating Chroma Docker Image..."
 
-docker build -f chroma/Dockerfile -t chroma-executor .
+docker build --platform=linux/amd64 -f chroma/Dockerfile -t chroma-executor .
 docker tag chroma-executor jhuguet/chroma-executor
 docker push jhuguet/chroma-executor
 
 echo "Building and updating Langchain Docker Image..."
 
-docker build -f user_langchain/Dockerfile -t langchain-executor .
+docker build --platform=linux/amd64  -f user_langchain/Dockerfile -t langchain-executor .
 docker tag langchain-executor jhuguet/langchain-executor
 docker push jhuguet/langchain-executor
