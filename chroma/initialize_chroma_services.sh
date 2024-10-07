5fa01a8628791451efa4798ce56cf1c084c5ab8e
jhugosp <juan.huguet@wizeline.com>
2024-10-07 16:21:20 -0500

    REFACTOR: structure

--- chroma/initialize_chroma_services.sh
+++ chroma/initialize_chroma_services.sh
@@ -1,5 +1,6 @@
-#!/bin/bash_files
+#!/bin/bash
 
-source /chroma_app/.chroma/bin/activate
+# Start Gunicorn in the background
 gunicorn -c chroma/gunicorn.conf.py 'chroma:create_app()' & \
+# Start Celery worker in the foreground
 celery -A chroma worker --loglevel=info --time-limit=50 --soft-time-limit=30 -Q chroma_queue --hostname=chroma@%h
