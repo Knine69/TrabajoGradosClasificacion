b2945d324db00dd4139e37b29bdaa353d7992e4f
jhugosp <juan.huguet@wizeline.com>
2024-10-09 17:36:57 -0500

    REFACTOR: soft time

--- chroma/initialize_chroma_services.sh
+++ chroma/initialize_chroma_services.sh
@@ -4,4 +4,4 @@ pip install -r chroma/requirements.txt
 # Start Gunicorn in the background
 gunicorn -c chroma/gunicorn.conf.py 'chroma:create_app()' & \
 # Start Celery worker in the foreground
-celery -A chroma worker --loglevel=info --time-limit=180 --soft-time-limit=180 -Q chroma_queue --hostname=chroma@%h
+celery -A chroma worker --loglevel=info --time-limit=120 --soft-time-limit=120 -Q chroma_queue --hostname=chroma@%h
