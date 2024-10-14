bea3d2a09b4003b26167131e5ee1ee333208b1dd
jhugosp <juan.huguet@wizeline.com>
2024-10-14 12:26:14 -0500

    REFACTOR: yield strategy

--- chroma/app/task_executor.py
+++ chroma/app/task_executor.py
@@ -4,20 +4,30 @@ import json
 from chroma.celery_conf import celery
 from chroma.app import redis_client
 from chroma.app.domain.chroma_collections import ChromaCollections
-from utils.outputs import print_successful_message
+from utils.outputs import print_successful_message, print_error
 from app_config import Configuration
 
 
 def sse_stream(task_id):
     """Stream events to the client."""
-    while True:
-        result = redis_client.get(task_id)
+    try:
+        while True:
+            # Poll Redis for task result
+            result = redis_client.get(task_id)
 
-        if result:
-            yield f"data: {result.decode('utf-8')}\n\n"
-            break
+            if result:
+                # Once the result is available, stream it to the client and exit
+                yield f"data: {result.decode('utf-8')}\n\n"
+                break  # End the loop after sending the result
 
-        time.sleep(2)  # Poll every 2 seconds
+            time.sleep(2)  # Poll every 2 seconds
+    except GeneratorExit:
+        print_error(f"Client disconnected while waiting for task {task_id}",
+                    app=Configuration.CHROMA_QUEUE)
+    except Exception as e:
+        print_error(f"Error during SSE streaming: {e}",
+                    app=Configuration.CHROMA_QUEUE)
+        yield f"data: Error while processing task {task_id}\n\n"
 
 
 @celery.task(soft_time_limit=30, time_limit=50)
