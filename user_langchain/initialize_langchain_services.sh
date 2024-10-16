ddf8b8f4351f84830b4d824b683c64fd6e060401
jhugosp <juan.huguet@wizeline.com>
2024-10-16 16:48:22 -0500

    REFACTOR: local initialization

--- user_langchain/initialize_langchain_services.sh
+++ user_langchain/initialize_langchain_services.sh
@@ -1,5 +1,5 @@
 #!/bin/bash_files
 
-source /langchain_app/.langchain/bin/activate
+# source /langchain_app/.langchain/bin/activate
 gunicorn -c user_langchain/gunicorn.conf.py 'user_langchain:create_app()' & \
 celery -A user_langchain worker --loglevel=debug  -Q langchain_queue -n langchain@%n
