a29b0e83b958b8b3bb4a0e8840a712ae2981822e
jhugosp <juan.huguet@wizeline.com>
2024-10-25 10:42:36 -0500

    ADD: waiting logic for llama service

--- bash_files/initialize_basic_services.sh
+++ bash_files/initialize_basic_services.sh
@@ -1,4 +1,14 @@
-#!bin/bash
+#!/bin/sh
 
-OLLAMA_SCHED_SPREAD=true OLLAMA_NUM_PARALLEL=2 OLLAMA_GPU_OVERHEAD=1024M ollama serve & \
+OLLAMA_SCHED_SPREAD=true OLLAMA_NUM_PARALLEL=2 OLLAMA_GPU_OVERHEAD=1024M ollama serve & 
+
+while ! nc -z localhost 11434; do
+    echo "Waiting for llama service"
+    sleep 2
+done
+
+echo "Llama is available"
+
+wait
+
 docker compose -f docker-compose.yml up rabbitmq redis chroma
