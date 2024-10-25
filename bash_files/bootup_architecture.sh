0212ee8b6d61ceb0e25d96e070b8d6013e362d86
jhugosp <juan.huguet@wizeline.com>
2024-10-25 10:42:45 -0500

    ADD: architecture boot

---
+++ bash_files/bootup_architecture.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "Starting basic Docker services..." 
+sh bash_files/initialize_basic_services.sh &
+
+ports=(5432 6379)
+
+# Function to check a single port
+check_port() {
+  local port=$1
+  while ! nc -z localhost $port; do
+    echo "Waiting for port $port to be ready..."
+    sleep 3
+  done
+  echo "Port $port is ready!"
+}
+
+for port in "${ports[@]}"; do
+  check_port $port &
+done
+
+wait
+
+echo "Starting Chroma Microservice"
+sh chroma/initialize_chroma_services.sh
