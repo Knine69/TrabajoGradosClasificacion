66c8b1b72880e6051f1b70324913d0895f43361d
jhugosp <juan.huguet@wizeline.com>
2024-11-04 14:16:47 -0500

    REFACTOR: comments

--- chroma/initialize_chroma_services.sh
+++ chroma/initialize_chroma_services.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-pip install -r chroma/requirements.txt
-# Start Gunicorn in the background
+
 gunicorn -c chroma/gunicorn.conf.py 'chroma:create_app()' & \
-# Start Celery worker in the foreground
 celery -A chroma worker --loglevel=info -Q chroma_queue --hostname=chroma@%h
