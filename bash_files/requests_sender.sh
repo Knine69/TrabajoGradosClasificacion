9e4e742ec71c54722a51efd3c46697a24d8e376f
jhugosp <juan.huguet@wizeline.com>
2024-11-05 15:07:56 -0500

    ADD: automated curl sender

---
+++ bash_files/requests_sender.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <input_file>"
+  exit 1
+fi
+
+input_file="$1"
+collection="gaunal_collection"
+values=("quimica" "control" "electronica" "mecanica" "electrica" "robotica")
+
+if [ ! -f "$input_file" ]; then
+  echo "Error: File '$input_file' not found."
+  exit 1
+fi
+
+while IFS= read -r path; do
+  curl -X POST 'http://localhost:5000/chroma/embed_document' \
+    -H 'Content-Type: application/json' \
+    -d "{\"collection_name\": \"$collection\", \"categories\": [\"${values[$RANDOM % ${#values[@]}]}\"], \"file_path\": \"$path\"}"
+
+  echo
+done < "$input_file"
