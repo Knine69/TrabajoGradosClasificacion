0a28bfae1b1898db8cd0d8ad5b13c1bd324136a7
jhugosp <juan.huguet@wizeline.com>
2024-11-05 18:28:00 -0500

    REFACTOR: time limit test

--- chroma/app/task_executor.py
+++ chroma/app/task_executor.py
@@ -72,7 +72,7 @@ def chroma_search_query_task(collection_name, category, user_query):
     return result
 
 
-@celery.task(soft_time_limit=240)
+# @celery.task(soft_time_limit=240)
 def chroma_embed_task(collection_name, file_path, categories):
 
     task_id = chroma_embed_task.request.id
