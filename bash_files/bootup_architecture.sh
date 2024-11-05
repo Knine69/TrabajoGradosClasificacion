69299ac8d93d9cdbdd576846893e403f5e6894e7
jhugosp <juan.huguet@wizeline.com>
2024-11-05 16:38:16 -0500

    REFACTOR: shebang

--- bash_files/bootup_architecture.sh
+++ bash_files/bootup_architecture.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting basic Docker services..." 
-sh bash_files/initialize_basic_services.sh &
+bash bash_files/initialize_basic_services.sh &
 
 ports=(5432 6379)
 
@@ -22,4 +22,4 @@ done
 wait
 
 echo "Starting Chroma Microservice"
-sh chroma/initialize_chroma_services.sh
+bash chroma/initialize_chroma_services.sh
