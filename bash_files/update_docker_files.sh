48b39280f321d5ad711a7864524e7fecc74bbb76
knine69 <jhuguetospina@gmail.com>
2024-12-03 18:38:43 -0500

    ADD: versioning to docker images

--- bash_files/update_docker_files.sh
+++ bash_files/update_docker_files.sh
@@ -2,6 +2,12 @@
 
 echo "Building and updating Langchain Docker Image..."
 
-docker build -f user_langchain/Dockerfile -t langchain-executor .
-docker tag langchain-executor jhuguet/langchain-executor
-docker push jhuguet/langchain-executor
+docker build -f user_langchain/Dockerfile -t jhuguet/langchain-executor:1.0.0 .
+docker tag langchain-executor:1.0.0 jhuguet/langchain-executor:latest
+docker push jhuguet/langchain-executor:1.0.0
+
+echo "Building and updating Chroma Docker Image..."
+
+docker build -f chroma/Dockerfile -t jhuguet/chroma-executor:1.0.0 .
+docker tag chroma-executor:1.0.0 jhuguet/chroma-executor:latest
+docker push jhuguet/chroma-executor:1.0.0
