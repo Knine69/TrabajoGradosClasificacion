7cb5848418e40b6d27771a3bb6f4f1d4d0795948
knine69 <jhuguetospina@gmail.com>
2024-12-04 16:24:54 -0500

    REFACTOR: CORS and server ip address exposure

--- chroma/gunicorn.conf.py
+++ chroma/gunicorn.conf.py
@@ -1,9 +1,8 @@
-bind = '127.0.0.1:5000'  # Address and port to bind to
+bind = '0.0.0.0:5000'  # Bind to all interfaces to allow external access
 workers = 2            # Number of worker processes
 threads = 4            # Number of threads per worker
 timeout = 120          # Timeout for requests
 accesslog = '-'        # Log requests to stdout
 errorlog = '-'         # Log errors to stdout
-loglevel = 'debug'      # Log level (can be debug, info, warning, error,
-# critical)
-preload_app = False
+loglevel = 'debug'     # Log level (debug, info, warning, error, critical)
+preload_app = False    # Do not preload the app
